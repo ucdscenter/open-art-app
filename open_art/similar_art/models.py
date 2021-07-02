fd52f61b4b43b173baf3b41147e23681e2c98a96
Ezra Edgerton <ezraedgerton@MacBook-Pro-68.local>
2021-07-02 11:38:52 -0500

    adding initial django structur

---
+++ open_art/similar_art/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Query(models.Model):
+    image_name = models.CharField(max_length=200)
+    date = models.DateTimeField('date uploaded')
+
+
+class Response(models.Model):
+    query = models.ForeignKey(Query, on_delete=models.CASCADE)
+    main_responses = models.CharField(max_length=200)
